166edd601eb74e9f1b38c93bb01ff780dcb438d5
Ashita-0204 <ashitavarshney37532@gmail.com>
2025-06-15 16:27:26 +0530

    DigitCounter Add . File Added

--- ControlFlow/Level03/DigitCounter.java
+++ ControlFlow/Level03/DigitCounter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DigitCounter {
+public class DigitCounter { 
     public static void main(String[] args) {
 	//Input
         Scanner sc = new Scanner(System.in);
