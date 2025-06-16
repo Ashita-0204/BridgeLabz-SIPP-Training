2a27c191809661e079109697be4807e0b399f546
Ashita-0204 <ashitavarshney37532@gmail.com>
2025-06-16 10:00:59 +0530

    [Ashita] Add . Leap Year using if

--- ControlFlow/Level03/LeapYearIf.java
+++ ControlFlow/Level03/LeapYearIf.java
@@ -1,6 +1,6 @@
  import java.util.Scanner;
 
-public class LeapYearIf {
+public class LeapYearIf { 
     public static void main(String[] args) {
 	//Input
         Scanner sc = new Scanner(System.in);
