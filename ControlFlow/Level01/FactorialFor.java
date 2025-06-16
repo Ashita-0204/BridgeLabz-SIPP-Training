93e6043c7c5d6a5e6ff48d28826b9d71801fe110
Ashita-0204 <ashitavarshney37532@gmail.com>
2025-06-16 09:32:33 +0530

    [Ashita] Add .  Factorial Using For

--- ControlFlow/Level01/FactorialFor.java
+++ ControlFlow/Level01/FactorialFor.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class FactorialFor {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
        //Input
         int n = sc.nextInt();
 //Loop
