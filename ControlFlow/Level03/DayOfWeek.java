972bf34d235ade58191a1681f7cff81780d39cf1
Ashita-0204 <ashitavarshney37532@gmail.com>
2025-06-16 09:58:54 +0530

    [Ashita] Add . Days Of Week

--- ControlFlow/Level03/DayOfWeek.java
+++ ControlFlow/Level03/DayOfWeek.java
@@ -1,5 +1,5 @@
 public class DayOfWeek {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         //Input
         int m = Integer.parseInt(args[0]); 
         int d = Integer.parseInt(args[1]); 
