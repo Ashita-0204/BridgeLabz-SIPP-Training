4e5f544e26c6e37e155563ace9913e6455b23132
Ashita-0204 <ashitavarshney37532@gmail.com>
2025-06-26 09:32:23 +0530

    $ git commit -m "[Ashita] Add . Bank Account Details via Access Modifiers"

--- OOPS/Constructors/AccessModifiers/BankAccount.java
+++ OOPS/Constructors/AccessModifiers/BankAccount.java
@@ -1,5 +1,32 @@
 package AccessModifiers;
 
-public class BankAccount {
+class BankAccount {
+    public int accountNumber;
+    protected String accountHolder;
+    private double balance;
 
+    BankAccount(int accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    SavingsAccount(int accountNumber, String accountHolder, double balance) {
+        super(accountNumber, accountHolder, balance);
+    }
+
+    void displayAccountInfo() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolder);
+    }
 }
